47ea20bd36e7d1e43a9458428679b80117284f4e
Łukasz Sadłocha <lukaszsadlocha@gmail.com>
2019-07-08 10:23:29 +0200

    Initial commit

---
+++ c_sharp-interview-questions-code-examples/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_interview_questions_code_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
